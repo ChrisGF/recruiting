425e0b4c330be6c271c87d20f1cb152f792be69d
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2014-05-26 13:44:27 -0400

    Quick Deals dashboard to show admins all the deals.

---
+++ app/admin/deal.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register Deal do
+  
+  permit_params :model
+
+  filter :state
+  filter :capital_type
+  filter :close_timeline
+  filter :amount_to_raise
+  
+  scope :all
+  scope :published
+  
+  index do
+    column :name
+    column :short_description
+    column :state
+    column :capital_type
+    column :close_timeline
+    column :amount_to_raise
+  end
+  
+end
