1cbd0493d506b665a81f18d63f05d18df552315b
Hunter Stewart <hetre85@gmail.com>
2014-08-07 11:50:55 -0500

    created spec_helper.rb and commented out uninitialized constants to run tests. 42 test failures

--- spec/helpers/developer_profiles_helper_spec.rb
+++ spec/helpers/developer_profiles_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe DeveloperProfilesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe DeveloperProfilesHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
