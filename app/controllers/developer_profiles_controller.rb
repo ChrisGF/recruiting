8b716dd86709ff84328b76b6469e4875f7f897a3
Hunter Stewart <hetre85@gmail.com>
2014-08-08 15:18:43 -0500

    Refactored developer_profiles controller with before_filters

--- app/controllers/developer_profiles_controller.rb
+++ app/controllers/developer_profiles_controller.rb
@@ -1,22 +1,19 @@
 class DeveloperProfilesController < InheritedResources::Base
   
+  before_filter :set_user
+  before_filter :set_developer_profile
+
   def next_steps  
   end
 
   def show
-    user = User.find(params[:user_id])
-    @developer_profile = user.developer_profile
   end
 
   def edit
-  	@user = User.find(params[:user_id])
-  	@developer_profile = @user.developer_profile
   end
 
   def update
-  	user = User.find(params[:user_id])
-  	@developer_profile = user.developer_profile
-  	if @developer_profile.update(developer_profile_params)
+    if @developer_profile.update(developer_profile_params)
   		redirect_to user_developer_profile_path, notice: "Profile updated!"
   	else
   		render :edit
@@ -25,9 +22,18 @@ class DeveloperProfilesController < InheritedResources::Base
 
 private
 
+  def set_user
+  	@user = User.find(params[:user_id])
+  end
+  
+  def set_developer_profile
+  	@developer_profile = @user.developer_profile
+  end
+
   def developer_profile_params
   	params.require(:developer_profile).permit(:name, :employment, :experience, :yearly_projects,
   	               :typical_project_size, :how_did_you_hear, :how_response, :why, :state)
   end
+
 
 end
