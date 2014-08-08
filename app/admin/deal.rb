b6aee721ab3761571411580aeb88992e990987db
Hunter Stewart <hetre85@gmail.com>
2014-08-08 15:21:09 -0500

    Added ActiveAdmin page for deals

---
+++ app/admin/deal.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Deal do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  controller do
+
+    def permitted_params
+      params.permit!
+    end
+  end
+  
+end
