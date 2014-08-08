3374ff70027547dec76ac013a64bb0985813140a
Hunter Stewart <hetre85@gmail.com>
2014-08-08 13:42:03 -0500

    Added ability for devs to view/update profiles. Nested resources and updated links

--- app/controllers/developer_profiles_controller.rb
+++ app/controllers/developer_profiles_controller.rb
@@ -4,7 +4,30 @@ class DeveloperProfilesController < InheritedResources::Base
   end
 
   def show
-    user = User.find(params[:id])
+    user = User.find(params[:user_id])
     @developer_profile = user.developer_profile
   end
+
+  def edit
+  	@user = User.find(params[:user_id])
+  	@developer_profile = @user.developer_profile
+  end
+
+  def update
+  	user = User.find(params[:user_id])
+  	@developer_profile = user.developer_profile
+  	if @developer_profile.update(developer_profile_params)
+  		redirect_to user_developer_profile_path, notice: "Profile updated!"
+  	else
+  		render :edit
+  	end
+  end
+
+private
+
+  def developer_profile_params
+  	params.require(:developer_profile).permit(:name, :employment, :experience, :yearly_projects,
+  	               :typical_project_size, :how_did_you_hear, :how_response, :why, :state)
+  end
+
 end
