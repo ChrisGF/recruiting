48fdc74a51f9f1f941a87429fb0c1a0f537fc365
Jason Lyles <lylesjt@gmail.com>
2014-09-29 07:49:00 -0400

    Moving STATES and VALID_STATES back into address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,6 +4,13 @@ class Address < ActiveRecord::Base
 
   before_create :default_name
 
+  #Could possibly move this to a config file or ENV var and be able to update without updating the app, but the point
+  # was to try and define this in one place, and then base the STATES used for the form off of it. That way, a user
+  # couldn't even select an invalid state, and we would just need to update VALID_STATES, and get STATES updated for
+  # the form for free.
+  VALID_STATES = ['GA']
+  STATES = VALID_STATES.collect{|value| [value,value]}.sort
+
   validates :state, presence: true
   validates :state, inclusion: {in: VALID_STATES, message: "%{value} is not a valid state. We currently only do business in the following states: #{VALID_STATES.to_sentence}"}
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Recruiting::Application.initialize!
-
-#could possibly also move this to a config file or ENV var, but the point was to try and define this in one place,
-#and then base the STATES used for the form off of it. That way, a user couldn't even select an invalid state, and
-# we would just need to update VALID_STATES, and get STATES updated for the form for free.
-VALID_STATES = ['GA']
-STATES = VALID_STATES.collect{|value| [value,value]}.sort
